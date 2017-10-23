93d221bb56abd355d50991a7e1a45ef67fc7f69e
AirDu <airfreedom5@gmail.com>
2017-10-23 22:41:23 +0800

    Markdown

--- dbxsite/website/views.py
+++ dbxsite/website/views.py
@@ -1,6 +1,7 @@
 # coding=utf-8
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
 from .models import Article
 
@@ -12,6 +13,7 @@ def index(request):
 
 def article_page(request, article_id):
     article = Article.objects.get(pk=article_id)
+    print(article.content)
     return render(request, 'website/article_page.html', {'article': article})
 
 
@@ -20,3 +22,18 @@ def article_edit_page(request, article_id):
         return render(request, 'website/article_edit_page.html')
     article = get_object_or_404(Article, pk=article_id)
     return render(request, 'website/article_edit_page.html', {'article': article})
+
+
+def edit_action(request):
+    title = request.POST.get('title', 'TITLE')
+    content = request.POST.get('content', 'CONTENT')
+    article_id = request.POST.get('article_id', '0')
+
+    if article_id == '0':
+        article = Article.objects.create(title=title, content=content)
+    else:
+        article = Article.objects.get(pk=article_id)
+        article.title = title
+        article.content = content
+        article.save()
+    return HttpResponseRedirect(reverse('article:article_page', args=[article.id]))
