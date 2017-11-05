69318a7f3d945c5c584d06736e207bc4c17e75d7
AirDu <airfreedom5@gmail.com>
2017-11-05 22:09:11 +0800

    User

--- dbxsite/usersys/models.py
+++ dbxsite/usersys/models.py
@@ -1,3 +1,17 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    related_id = models.CharField(max_length=128, null=True)
+    user = models.OneToOneField(User)
+
+    def __str__(self):
+        return self.user.username
+
+
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        profile = UserProfile()
+        profile.user = instance
+        profile.save()
