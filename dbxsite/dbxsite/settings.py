9e71347e40238e1a798af5ac3b4e8fb8bf5adea9
root <airfreedom5@gmail.com>
2020-08-12 20:25:39 +0800

    Added .gitignore

--- dbxsite/dbxsite/settings.py
+++ dbxsite/dbxsite/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'Website',
         'USER': 'site',
-        'PASSWORD': 'password',
+        'PASSWORD': 'Z*meng521',
         'HOST': 'localhost',
         'PORT': 3306
     }
